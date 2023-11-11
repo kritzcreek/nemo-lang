12478d4cb6498bc3f8b607c1d8685d58d0dd9cc6
Christoph Hegemann <christoph@deepchannel.com>
2023-11-11 10:22:33 +0000

    updates comment on IR structure

--- packages/nemo-backend/src/ir.rs
+++ packages/nemo-backend/src/ir.rs
@@ -48,8 +48,8 @@ pub struct FuncTy {
 // 2. All syntactic type annotations are omitted, as we only care about the infered/semantic ones
 // 3. All type-based resolution is done during lowering -> + becomes f32.+ or i32.+ and all struct indexes are
 //    unique based on their type
-// 4. The only desugaring we do at this point in time is to simplify set_targets to identifier + index pairs
-//    eg: `set x.y.z = 1`` => `let $gen = x.y; set $gen.z = 1`
+// 4. The only desugaring we do at this point in time is to transform set_targets to expr + index pairs, which
+//    which makes it easier to generate code for these eventually
 //
 // We also try to keep as many Spans around as possible, mostly to help us when debugging our compiler
 
