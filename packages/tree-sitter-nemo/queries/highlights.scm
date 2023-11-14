bea6c33840bbbe6eb442827c3728e10cf36ef54c
Christoph Hegemann <christoph@deepchannel.com>
2023-11-14 15:40:43 +0100

    adds highlight queries for tree-sitter

---
+++ packages/tree-sitter-nemo/queries/highlights.scm
@@ -0,0 +1,43 @@
+"struct" @keyword
+"fn" @keyword
+"let" @keyword
+"set" @keyword
+"import" @keyword
+"from" @keyword
+"if" @keyword
+"else" @keyword
+"while" @keyword
+
+(ty_i32) @type
+(ty_f32) @type
+(ty_bool) @type
+(ty_unit) @type
+(ty_array) @type
+(ty_struct) @type
+(struct_e struct: (upper_ident) @type)
+
+(top_func name: (lower_ident) @function)
+(call_e function: (lower_ident) @function)
+(intrinsic_ident) @function
+
+(struct_field_e name: (lower_ident) @property)
+(struct_field_top name: (lower_ident) @property)
+(struct_idx_e index: (lower_ident) @property)
+(set_struct_idx index: (lower_ident) @property)
+
+(float_lit) @literal
+(int_lit) @literal
+(bool_lit) @literal
+
+"&&" @operator
+"||" @operator
+"==" @operator
+"!=" @operator
+"<" @operator
+"<=" @operator
+">" @operator
+">=" @operator
+"+" @operator
+"-" @operator
+"*" @operator
+"/" @operator
