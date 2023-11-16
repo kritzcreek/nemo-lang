f6d2a264c56d7b63e36ad83b2d80bfa1e2c0fcf6
Christoph Hegemann <christoph@deepchannel.com>
2023-11-16 18:34:08 +0100

    creates a unified cli for nemo

--- packages/nemo-frontend/src/lib.rs
+++ packages/nemo-frontend/src/lib.rs
@@ -8,9 +8,14 @@ pub mod types;
 use parser::parse_program;
 use types::typecheck;
 
-pub fn check_program(program: &str) -> types::TyResult<syntax::Program> {
-    let parse_tree = parse_program(program);
-    typecheck(program, parse_tree.root_node())
+pub fn check_program(source: &str) -> types::TyResult<syntax::Program> {
+    let parse_tree = parse_program(source);
+    typecheck(source, parse_tree.root_node())
+}
+
+pub fn print_program(program: &syntax::Program) -> String {
+    let printer = pretty::Printer::new(true);
+    printer.print_program(program)
 }
 
 #[cfg(test)]
--- packages/nemo-frontend/src/main.rs
+++
@@ -1,7 +0,0 @@
-use nemo_frontend::{parser::parse_program, types::typecheck};
-
-fn main() {
-    let source = "let x : i32 = 10.0";
-    let parsed = parse_program(&source);
-    typecheck(source, parsed.root_node()).unwrap();
-}
