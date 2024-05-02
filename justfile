c2e3bb7f8364e19a246aef3a45ae60c933b354b0
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2024-05-02 13:19:47 +0200

    Adds compiled Wasm instance into codemirror statefield

--- justfile
+++ justfile
@@ -1,13 +1,17 @@
 build-cli:
-  cargo build --release --bin nemo
+    cargo build --release --bin nemo
+
+build-wasm-lib:
+    cd packages/wasm-lib && cargo rustc  --crate-type cdylib --target wasm32-unknown-unknown --release
+    wasm-bindgen target/wasm32-unknown-unknown/release/wasm_lib.wasm --out-dir playground/wasm-lib/ --target web
 
 @run-wasm FILE:
-  mkdir -p build
-  ./target/release/nemo compile {{FILE}} --output build/{{ without_extension(FILE) }}.wasm
-  wasm-opt -O3 build/{{ without_extension(FILE) }}.wasm -o build/{{ without_extension(FILE) }}_opt.wasm
-  wasm-tools print build/{{ without_extension(FILE) }}.wasm -o build/{{ without_extension(FILE) }}.wast
-  wasm-tools print build/{{ without_extension(FILE) }}_opt.wasm -o build/{{ without_extension(FILE) }}_opt.wast
-  node dev/run-wasm.mjs build/{{ without_extension(FILE) }}.wasm
+    mkdir -p build
+    ./target/release/nemo compile {{ FILE }} --output build/{{ without_extension(FILE) }}.wasm
+    wasm-opt -O3 build/{{ without_extension(FILE) }}.wasm -o build/{{ without_extension(FILE) }}_opt.wasm
+    wasm-tools print build/{{ without_extension(FILE) }}.wasm -o build/{{ without_extension(FILE) }}.wast
+    wasm-tools print build/{{ without_extension(FILE) }}_opt.wasm -o build/{{ without_extension(FILE) }}_opt.wast
+    node dev/run-wasm.mjs build/{{ without_extension(FILE) }}.wasm
 
 dev FILE: build-cli
-  watchexec --quiet -e nemo just run-wasm {{FILE}}
+    watchexec --quiet -e nemo just run-wasm {{ FILE }}
--- playground/src/editor.ts
+++ playground/src/editor.ts
@@ -1,56 +1,33 @@
 import "./style.css";
 
+import { html, render } from "lit-html";
 import { EditorView, minimalSetup } from "codemirror";
-import { autocompletion, closeBrackets } from "@codemirror/autocomplete";
+import { closeBrackets } from "@codemirror/autocomplete";
 import {
   ViewPlugin,
   DecorationSet,
   Decoration,
   ViewUpdate,
   lineNumbers,
+  showPanel,
+  Panel,
+  panels,
 } from "@codemirror/view";
 import { linter, Diagnostic } from "@codemirror/lint";
-import { Extension, RangeSetBuilder } from "@codemirror/state";
+import {
+  EditorState,
+  Extension,
+  RangeSetBuilder,
+  StateField,
+  Transaction,
+} from "@codemirror/state";
 import * as compiler from "../wasm-lib/wasm_lib.js";
 
-const base_theme = EditorView.baseTheme({
-  ".cm-literal": { color: "#3548cf" },
-  ".cm-keyword": { color: "#5317ac" },
-  ".cm-column": { color: "#00538b" },
-  ".cm-table": { color: "#721045" },
-  ".cm-func": { fontStyle: "italic" },
-  ".cm-recover": {
-    background: "rgba(255, 220, 220, 0.5)",
-    color: "rgba(0, 0, 0, 0.5)",
-  },
-});
-
-const literal_decoration = Decoration.mark({
-  class: "cm-literal",
-});
-const keyword_decoration = Decoration.mark({
-  class: "cm-keyword",
-});
-const column_decoration = Decoration.mark({
-  class: "cm-column",
-});
-const table_decoration = Decoration.mark({
-  class: "cm-table",
-});
-const func_decoration = Decoration.mark({
-  class: "cm-func",
-});
-const recover_decoration = Decoration.mark({
-  class: "cm-recover",
-});
+// Hacky
+let imports: WebAssembly.Imports = undefined as any;
 
 function highlight_view(view: EditorView) {
   let builder = new RangeSetBuilder<Decoration>();
-  let input = view.state.doc.toString();
-  let result = compiler.compile(input);
-  for (const error of result.errors) {
-    console.log(error.message);
-  }
   // let { root } = run_parse(input, 10000);
   // if (!root) {
   //   return builder.finish();
@@ -86,9 +63,8 @@ function highlight_view(view: EditorView) {
 
 const nemoLinter = linter((view) => {
   let diagnostics: Diagnostic[] = [];
-  let input = view.state.doc.toString();
-  let result = compiler.compile(input);
-  for (const error of result.errors) {
+  let result = view.state.field(compile_result);
+  for (const error of result.diagnostics) {
     diagnostics.push({
       from: error.start,
       to: error.end,
@@ -117,24 +93,112 @@ const highlight_plugin = ViewPlugin.fromClass(
 );
 
 function highlight_nemo(): Extension {
-  return [base_theme, highlight_plugin];
+  return [highlight_plugin];
 }
+
+type CompileState = {
+  diagnostics: compiler.Diagnostic[];
+  instance: WebAssembly.Instance | undefined;
+};
+
+function compile(input: string, imports: WebAssembly.Imports): CompileState {
+  const result = compiler.compile(input);
+  const diagnostics = result.errors;
+  let instance;
+  if (diagnostics.length === 0) {
+    try {
+      // Using synchronous module instantiation
+      // (not recommended, but makes working with the Codemirror API much simpler)
+      const mod = new WebAssembly.Module(result.wasm);
+      instance = new WebAssembly.Instance(mod, imports);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return { diagnostics, instance };
+}
+
+const compile_result = StateField.define<CompileState>({
+  create: function (state: EditorState): CompileState {
+    return compile(state.doc.toString(), imports);
+  },
+  update: function (
+    prev: CompileState,
+    transaction: Transaction,
+  ): CompileState {
+    return transaction.docChanged
+      ? compile(transaction.newDoc.toString(), imports)
+      : prev;
+  },
+});
+
+function actions_panel(_view: EditorView): Panel {
+  function render_buttons(
+    element: HTMLElement,
+    instance: WebAssembly.Instance | undefined,
+  ) {
+    let active = instance != null;
+    let button_bar = html`<button
+      id="runBtn"
+      ?disabled=${!active}
+      @click=${() => console.log((instance?.exports as any).main())}
+    >
+      Run
+    </button>`;
+    render(button_bar, element);
+  }
+  const dom = document.createElement("div");
+  render_buttons(dom, undefined);
+  return {
+    dom,
+    update(view_update) {
+      if (view_update.docChanged) {
+        const result = view_update.state.field(compile_result);
+        render_buttons(dom, result.instance);
+      }
+    },
+  };
+}
+
+function canvas_panel(view: EditorView): Panel {
+  function render_canvas(element: HTMLElement) {
+    const canvas = html`<canvas id="canvas" width="500" height="500"></canvas>`;
+    render(canvas, element);
+  }
+
+  let dom = document.createElement("div");
+  render_canvas(dom);
+  return {
+    dom,
+    async update(view_update) {},
+    top: true,
+  };
+}
+
+const actions = showPanel.of(actions_panel);
+const canvas = showPanel.of(canvas_panel);
 
 const initial_code =
   JSON.parse(localStorage.getItem("code") ?? '""') ||
   "fn main() : i32 = {\n  20 * 2 + 2\n}";
 
-export function setupEditor() {
+export function setupEditor(imports: WebAssembly.Imports) {
+  imports = imports;
   new EditorView({
     doc: initial_code,
     extensions: [
       minimalSetup,
+      panels({
+        topContainer: document.getElementById("canvas-container") ?? undefined,
+      }),
       lineNumbers(),
-      autocompletion({ override: [] }),
+      compile_result,
+      canvas,
+      actions,
       highlight_nemo(),
       closeBrackets(),
       nemoLinter,
     ],
-    parent: document.getElementById("editor")!,
+    parent: document.getElementById("editor-container")!,
   });
 }
