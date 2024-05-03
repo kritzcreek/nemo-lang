2fe8dc081f3f5b2a9d896b3c3ef409496d6f2af6
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2024-05-03 17:38:30 +0200

    Update playground

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   assetsInclude: ["examples/*.nemo"],
-  base: "https://kritzcreek.github.io/nemo-lang/",
+  base: "/nemo-lang/",
 })
