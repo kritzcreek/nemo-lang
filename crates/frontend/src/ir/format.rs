ba84d848fe07485ef2aa80fbc979b69ae0a51068
Christoph <christoph.hegemann@sourcegraph.com>
2024-07-13 04:42:49 +0200

    rustfmt

--- crates/frontend/src/ir/format.rs
+++ crates/frontend/src/ir/format.rs
@@ -1,5 +1,8 @@
+use crate::ir::{
+    Callee, Declaration, DeclarationData, Expr, ExprData, LitData, Name, NameSupply, Program,
+    SetTarget, SetTargetData, Ty,
+};
 use lexpr::Value;
-use crate::ir::{Callee, Declaration, DeclarationData, Expr, ExprData, LitData, Name, NameSupply, Program, SetTarget, SetTargetData, Ty};
 
 pub fn format_ir(names: &NameSupply, ir: &Program) -> String {
     format!("{}", Formatter { names }.program(ir))
@@ -30,36 +33,41 @@ impl Formatter<'_> {
                 self.expr(target),
                 self.name(index),
             ]),
-            SetTargetData::SetVar { name } => Value::list(vec![
-                Value::symbol("set_var"),
-                self.name(name),
-            ])
+            SetTargetData::SetVar { name } => {
+                Value::list(vec![Value::symbol("set_var"), self.name(name)])
+            }
         }
     }
 
     fn decl(&self, decl: &Declaration) -> Value {
         match &decl.it {
-            DeclarationData::Let { binder, expr } => {
-                Value::list(vec![Value::symbol("let"), self.name(binder), self.expr(expr)])
-            }
-            DeclarationData::Set { set_target, expr } =>
-                Value::list(vec![Value::symbol("set"), self.set_target(set_target), self.expr(expr)]),
+            DeclarationData::Let { binder, expr } => Value::list(vec![
+                Value::symbol("let"),
+                self.name(binder),
+                self.expr(expr),
+            ]),
+            DeclarationData::Set { set_target, expr } => Value::list(vec![
+                Value::symbol("set"),
+                self.set_target(set_target),
+                self.expr(expr),
+            ]),
             DeclarationData::Expr { expr } => self.expr(expr),
-            DeclarationData::While { condition, body } =>
-                Value::list(vec![Value::symbol("while"), self.expr(condition), self.expr(body)]),
+            DeclarationData::While { condition, body } => Value::list(vec![
+                Value::symbol("while"),
+                self.expr(condition),
+                self.expr(body),
+            ]),
         }
     }
 
     fn expr(&self, e: &Expr) -> Value {
         match e.it.as_ref() {
-            ExprData::Lit { lit } => {
-                match lit.it {
-                    LitData::I32(i) => i.into(),
-                    LitData::F32(f) => f.into(),
-                    LitData::Bool(b) => b.into(),
-                    LitData::Unit => Value::symbol("unit")
-                }
-            }
+            ExprData::Lit { lit } => match lit.it {
+                LitData::I32(i) => i.into(),
+                LitData::F32(f) => f.into(),
+                LitData::Bool(b) => b.into(),
+                LitData::Unit => Value::symbol("unit"),
+            },
             ExprData::Var { name } => self.name(name),
             ExprData::Call { func, arguments } => {
                 let mut elems: Vec<Value> = vec![];
@@ -67,7 +75,9 @@ impl Formatter<'_> {
                     Callee::Func { name, type_args } => {
                         elems.push(self.name(name));
                         if !type_args.is_empty() {
-                            elems.push(Value::vector(type_args.tys().into_iter().map(|t| self.ty(t))))
+                            elems.push(Value::vector(
+                                type_args.tys().into_iter().map(|t| self.ty(t)),
+                            ))
                         }
                     }
                     Callee::FuncRef(e) => elems.push(self.expr(e)),
@@ -76,12 +86,11 @@ impl Formatter<'_> {
                 elems.extend(arguments.into_iter().map(|e| self.expr(e)));
                 Value::list(elems)
             }
-            ExprData::Binary { op, left, right } =>
-                Value::list(vec![
-                    Value::symbol(format!("{:?}", op.it)),
-                    self.expr(left),
-                    self.expr(right),
-                ]),
+            ExprData::Binary { op, left, right } => Value::list(vec![
+                Value::symbol(format!("{:?}", op.it)),
+                self.expr(left),
+                self.expr(right),
+            ]),
             ExprData::Array { elems } => {
                 let mut els = vec![Value::symbol("array")];
                 els.extend(elems.into_iter().map(|e| self.expr(e)));
@@ -92,7 +101,11 @@ impl Formatter<'_> {
                 self.expr(array),
                 self.expr(index),
             ]),
-            ExprData::If { condition, then_branch, else_branch } => Value::list(vec![
+            ExprData::If {
+                condition,
+                then_branch,
+                else_branch,
+            } => Value::list(vec![
                 Value::symbol("if"),
                 self.expr(condition),
                 self.expr(then_branch),
@@ -106,7 +119,11 @@ impl Formatter<'_> {
             }
             ExprData::Struct { name, fields } => {
                 let mut elems = vec![self.name(name)];
-                elems.extend(fields.into_iter().map(|(name, expr)| Value::from((self.name(name), self.expr(expr)))));
+                elems.extend(
+                    fields
+                        .into_iter()
+                        .map(|(name, expr)| Value::from((self.name(name), self.expr(expr)))),
+                );
                 Value::list(elems)
             }
             ExprData::StructIdx { expr, index } => Value::list(vec![
@@ -115,15 +132,28 @@ impl Formatter<'_> {
                 self.name(index),
             ]),
             ExprData::Match { .. } => "match".into(),
-            ExprData::Lambda { captures, params, return_ty, body } => {
+            ExprData::Lambda {
+                captures,
+                params,
+                return_ty,
+                body,
+            } => {
                 let mut elems = vec![Value::symbol("lambda")];
-                elems.push(Value::vector(captures.into_iter().map(|(n, ty)| Value::from((self.name(n), self.ty(ty))))));
-                elems.push(Value::list(params.into_iter().map(|(n, ty)| Value::from((self.name(n), self.ty(ty))))));
+                elems.push(Value::vector(
+                    captures
+                        .into_iter()
+                        .map(|(n, ty)| Value::from((self.name(n), self.ty(ty)))),
+                ));
+                elems.push(Value::list(
+                    params
+                        .into_iter()
+                        .map(|(n, ty)| Value::from((self.name(n), self.ty(ty)))),
+                ));
                 elems.push(self.ty(return_ty));
                 elems.push(self.expr(body));
                 Value::list(elems)
             }
-            ExprData::Return { expr } => Value::list(vec!["return".into(), self.expr(expr)])
+            ExprData::Return { expr } => Value::list(vec!["return".into(), self.expr(expr)]),
         }
     }
     fn program(&self, program: &Program) -> Value {
