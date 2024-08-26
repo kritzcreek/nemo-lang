ce87e767d2eb1c7131d424e7311b789252cf1d19
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2024-08-26 11:53:56 +0200

    got something that compiles

--- crates/frontend/src/lib.rs
+++ crates/frontend/src/lib.rs
@@ -7,6 +7,9 @@ pub mod parser;
 pub mod syntax;
 pub mod types;
 
+use std::collections::HashMap;
+
+use camino::{Utf8Path, Utf8PathBuf};
 use ir::{Ctx, ModuleId, ModuleIdGen, Program};
 use parser::{parse_prog, ParseError};
 
@@ -53,7 +56,10 @@ impl FrontendResult {
     pub fn display(&self, source: &str) {
         for module in &self.modules {
             for error in &module.ty_errors {
-                eprintln!("{}", error.display(source, &self.ctx, true));
+                eprintln!(
+                    "{}",
+                    error.display(&self.ctx, Utf8Path::new(""), source, true)
+                );
             }
         }
     }
@@ -72,6 +78,156 @@ impl FrontendResult {
             }
         }
         (self.ctx, ir)
+    }
+}
+
+#[derive(Debug)]
+pub struct FrontendResultNew<'src> {
+    pub ctx: Ctx,
+    pub modules: Vec<ModuleResult<'src>>,
+}
+
+impl FrontendResultNew<'_> {
+    pub fn errors(&self) -> impl Iterator<Item = CheckError> {
+        self.modules.iter().flat_map(|module| {
+            module.ty_errors.iter().map(CheckError::TypeError).chain(
+                module
+                    .parse_result
+                    .parse_errors
+                    .iter()
+                    .map(CheckError::ParseError),
+            )
+        })
+    }
+
+    pub fn display_errors(&self) -> Option<i32> {
+        let mut count = 0;
+        for module in &self.modules {
+            let path = module.parse_result.path.as_path();
+            for error in &module.parse_result.parse_errors {
+                count += 1;
+                println!("{}", error.display(path, module.parse_result.source, true));
+            }
+            for error in &module.ty_errors {
+                count += 1;
+                println!(
+                    "{}",
+                    error.display(&self.ctx, path, module.parse_result.source, true)
+                );
+            }
+        }
+        if count > 0 {
+            Some(count)
+        } else {
+            None
+        }
+    }
+
+    pub fn consume(self) -> (Ctx, Option<ir::Program>) {
+        let mut ir = ir::Program::default();
+        for module in self.modules {
+            if let Some(module_ir) = module.ir {
+                ir.merge(module_ir)
+            } else {
+                return (self.ctx, None);
+            }
+        }
+
+        return (self.ctx, Some(ir));
+    }
+}
+
+#[derive(Debug)]
+pub struct ModuleResult<'src> {
+    pub parse_result: ModuleParseResult<'src>,
+    pub occurrences: OccurrenceMap,
+    pub ty_errors: Vec<TyError>,
+    pub ir: Option<Program>,
+}
+
+#[derive(Debug)]
+pub struct ModuleParseResult<'src> {
+    pub id: ModuleId,
+    pub parse_errors: Vec<ParseError>,
+    pub source: &'src str,
+    pub parse: Module,
+    pub path: Utf8PathBuf,
+    pub name: String,
+    pub dependencies: Vec<String>,
+}
+
+pub fn run_frontend_new(sources: &[(Utf8PathBuf, String)]) -> FrontendResultNew {
+    let mut id_gen = ModuleIdGen::new();
+    let mut parsed_modules: HashMap<ModuleId, ModuleParseResult> = HashMap::new();
+    for (path, source) in sources {
+        let id = id_gen.next_id();
+        let (parse_root, parse_errors) = parse_prog(source).take();
+        let parse = parse_root
+            .modules()
+            .next()
+            .expect("Failed to parse a module from file: {path}");
+        let module_header = parse.mod_header().expect("Expected a module header");
+        let name = module_header
+            .ident_token()
+            .expect("Expected a module name")
+            .text()
+            .to_string();
+        let dependencies = module_header
+            .mod_uses()
+            .filter_map(|mod_use| mod_use.ident_token().map(|t| t.text().to_string()))
+            .collect();
+        parsed_modules.insert(
+            id,
+            ModuleParseResult {
+                id,
+                source,
+                name,
+                dependencies,
+                parse_errors,
+                parse,
+                path: path.clone(),
+            },
+        );
+    }
+    let module_sort_input: Vec<_> = parsed_modules
+        .values()
+        .map(|parsed_module| {
+            (
+                parsed_module.id,
+                parsed_module.name.as_str(),
+                parsed_module.dependencies.as_slice(),
+            )
+        })
+        .collect();
+    let sorted_modules = module_dag::toposort_modules_new(module_sort_input);
+
+    let mut ctx = Ctx::new(sources.len() as u16);
+    let mut checked_ids = vec![];
+    let mut checked_modules = vec![];
+    for id in sorted_modules {
+        let parsed_module = parsed_modules.remove(&id).unwrap();
+        let check_result = types::check_module(&ctx, parsed_module.parse.clone(), id, &checked_ids);
+        ctx.set_module_name(id, parsed_module.name.clone());
+        ctx.set_interface(id, check_result.interface);
+        ctx.set_name_supply(id, check_result.names);
+        if parsed_module.parse_errors.is_empty()
+            && check_result.errors.is_empty()
+            && check_result.ir.is_none()
+        {
+            panic!("No IR generated, despite no errors")
+        };
+        checked_ids.push(id);
+        checked_modules.push(ModuleResult {
+            parse_result: parsed_module,
+            occurrences: check_result.occurrences,
+            ty_errors: check_result.errors,
+            ir: check_result.ir,
+        });
+    }
+
+    FrontendResultNew {
+        ctx,
+        modules: checked_modules,
     }
 }
 
@@ -121,6 +277,13 @@ pub fn run_frontend(source: &str) -> FrontendResult {
     }
 }
 
+pub fn check_program_new(sources: &[(Utf8PathBuf, String)]) -> Result<(), String> {
+    let check_result = run_frontend_new(sources);
+    if let Some(count) = check_result.display_errors() {
+        return Err(format!("Check failed with {} errors", count));
+    }
+    return Ok(());
+}
 /// Checks the given program, and prints any parse or type errors.
 /// If there are any errors returns a summary message in Err otherwise Ok.
 pub fn check_program(source: &str) -> Result<(), String> {
@@ -129,7 +292,10 @@ pub fn check_program(source: &str) -> Result<(), String> {
         let mut count = 0;
         for err in check_result.errors() {
             count += 1;
-            eprintln!("{}", err.display(source, &check_result.ctx, true));
+            eprintln!(
+                "{}",
+                err.display(&check_result.ctx, Utf8Path::new(""), source, true)
+            );
         }
         return Err(format!("Check failed with {} errors", count));
     }
--- crates/frontend/src/module_dag.rs
+++ crates/frontend/src/module_dag.rs
@@ -14,6 +14,26 @@ fn mod_name(module: &Module) -> String {
         .unwrap_or("DEFAULT_MODULE_NAME".to_string())
 }
 
+pub fn toposort_modules_new(modules: Vec<(ModuleId, &str, &[String])>) -> Vec<ModuleId> {
+    let mut module_name_map = HashMap::new();
+    let mut module_id_map = HashMap::new();
+    let mut ts: TopologicalSort<ModuleId> = TopologicalSort::new();
+
+    for (id, name, _) in &modules {
+        ts.insert(*id);
+        module_name_map.insert(*name, *id);
+        module_id_map.insert(*id, *name);
+    }
+
+    for (id, _, deps) in &modules {
+        for dep in *deps {
+            let import_id = module_name_map[dep.as_str()];
+            ts.add_dependency(import_id, *id)
+        }
+    }
+    ts.collect()
+}
+
 pub fn toposort_modules(root: Root) -> Vec<(ModuleId, String, Module)> {
     let mut id_gen = ModuleIdGen::new();
     let (module_name_map, mut module_id_map) = {
