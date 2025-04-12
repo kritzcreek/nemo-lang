1a85a62cb25d279ed14da26ffb35cc34a82b418c
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2025-04-12 13:57:09 +0200

    test run tests with wasmtime

--- crates/cli/tests/lib.rs
+++ crates/cli/tests/lib.rs
@@ -1,8 +1,23 @@
+use backend::compile_program;
 use camino::{Utf8Path, Utf8PathBuf};
-use insta::{glob, Settings};
+use insta::{assert_snapshot, glob, Settings};
 use insta_cmd::{assert_cmd_snapshot, get_cargo_bin};
+use std::fmt;
 use std::process::Command;
 use std::{fs, path::Path};
+use wasmtime::{Caller, Config, Engine, Linker, Module, Store};
+
+fn read_source_files(
+    input_files: Vec<Utf8PathBuf>,
+) -> Result<Vec<(Utf8PathBuf, String)>, std::io::Error> {
+    input_files
+        .into_iter()
+        .map(|input_file| {
+            let source = fs::read_to_string(&input_file)?;
+            Ok((input_file, source))
+        })
+        .collect()
+}
 
 fn render_slash_path(path: &Utf8Path) -> String {
     assert!(path.is_relative());
@@ -59,6 +74,95 @@ fn collect_test_input(path: &Path) -> (String, Vec<Utf8PathBuf>) {
     (out_name.to_string(), paths)
 }
 
+enum Run {
+    Success { result: i32, output: Vec<String> },
+    Trap { reason: String, output: Vec<String> },
+}
+
+impl fmt::Display for Run {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Run::Success { result, output } => {
+                writeln!(f, "Success: {result}")?;
+                for out in output {
+                    writeln!(f, "{out}")?;
+                }
+            }
+            Run::Trap { reason, output } => {
+                writeln!(f, "Trapped: {reason}")?;
+                for out in output {
+                    writeln!(f, "{out}")?;
+                }
+            }
+        };
+        Ok(())
+    }
+}
+
+fn wasmtime_runner(wasm: Vec<u8>) -> Run {
+    let engine = Engine::new(Config::new().wasm_function_references(true).wasm_gc(true)).unwrap();
+    let mut store = Store::new(&engine, vec![]);
+    let mut linker = Linker::new(&engine);
+    let module = Module::new(&engine, &wasm).unwrap();
+    linker
+        .func_wrap("env", "log", |mut caller: Caller<'_, Vec<String>>, param: i32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        })
+        .unwrap();
+    linker
+        .func_wrap("env", "log_int", |mut caller: Caller<'_, Vec<String>>, param: i32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        })
+        .unwrap();
+    linker
+        .func_wrap("env", "log_float", |mut caller: Caller<'_, Vec<String>>, param: f32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        })
+        .unwrap();
+    linker
+        .func_wrap("env", "log_f32", |mut caller: Caller<'_, Vec<String>>, param: f32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        })
+        .unwrap();
+    linker
+        .func_wrap("env", "print_char", |mut caller: Caller<'_, Vec<String>>, param: u32| {
+            let mut last_line = caller.data_mut().pop().unwrap_or_default();
+            last_line.push_str(&format!("{}", char::from_u32(param).unwrap()));
+            caller.data_mut().push(last_line);
+            0
+        })
+        .unwrap();
+    linker
+        .func_wrap("env", "random", |_: i32| -> f32 { 0.0 })
+        .unwrap();
+
+    let instance = linker.instantiate(&mut store, &module).unwrap();
+    let main_export = instance.exports(&mut store).find(|export| export.name().ends_with("main")).unwrap();
+    let main_func = main_export.into_func().unwrap().typed::<(), i32>(&mut store).unwrap();
+    match main_func.call(&mut store, ()) {
+        Ok(result) => Run::Success {
+            result,
+            output: store.into_data(),
+        },
+        Err(err) => Run::Trap {
+            reason: err.to_string(),
+            output: store.into_data(),
+        },
+    }
+}
+
+fn run_wasmtime(path: &Path) {
+    let (_, paths) = collect_test_input(path);
+    let sources = read_source_files(paths).unwrap();
+    let wasm = compile_program(&sources).unwrap();
+    let output = wasmtime_runner(wasm);
+    assert_snapshot!(output)
+}
+
 fn run(path: &Path) {
     let (out_name, paths) = collect_test_input(path);
     let (args, out_path) = compile_args(&paths, &out_name);
@@ -99,6 +203,6 @@ fn t() {
     });
     glob!("run/*", |path| {
         let path = path.strip_prefix(&cwd).unwrap();
-        run(path);
+        run_wasmtime(path);
     });
 }
