4ee9d5770cea0d6f118e45774ca03c8044e3bca4
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2025-04-12 15:42:12 +0200

    clean up tests more

--- crates/cli/tests/lib.rs
+++ crates/cli/tests/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::{anyhow, Context, Result};
 use backend::compile_program;
 use camino::{Utf8Path, Utf8PathBuf};
 use insta::{assert_snapshot, glob};
@@ -7,9 +8,7 @@ use std::process::Command;
 use std::{fs, path::Path};
 use wasmtime::{Caller, Config, Engine, Linker, Module, Store};
 
-fn read_source_files(
-    input_files: Vec<Utf8PathBuf>,
-) -> Result<Vec<(Utf8PathBuf, String)>, std::io::Error> {
+fn read_source_files(input_files: Vec<Utf8PathBuf>) -> Result<Vec<(Utf8PathBuf, String)>> {
     input_files
         .into_iter()
         .map(|input_file| {
@@ -37,20 +36,18 @@ fn cli() -> Command {
     Command::new(get_cargo_bin("nemo"))
 }
 
-fn collect_test_input(path: &Path) -> (String, Vec<Utf8PathBuf>) {
-    let path = Utf8Path::from_path(path).expect("Non-utf8 path: {path:?}");
-    let out_name = path.file_stem().unwrap();
+fn collect_test_input(path: &Path) -> Result<Vec<Utf8PathBuf>> {
+    let path = Utf8Path::from_path(path).unwrap();
     let mut paths = vec![];
     if path.is_dir() {
-        for entry in fs::read_dir(path).expect("Failed to list directory {path:?}") {
-            let entry = entry.expect("Failed to read entry");
-            let path = entry.path();
-            paths.push(Utf8PathBuf::from_path_buf(path).expect("non-utf8 path: {path:?}"));
+        for entry in fs::read_dir(path)? {
+            let path = entry?.path();
+            paths.push(Utf8PathBuf::from_path_buf(path).unwrap());
         }
     } else {
         paths.push(path.to_owned());
     };
-    (out_name.to_string(), paths)
+    Ok(paths)
 }
 
 enum Run {
@@ -78,78 +75,65 @@ impl fmt::Display for Run {
     }
 }
 
-fn wasmtime_runner(wasm: Vec<u8>) -> Run {
-    let engine = Engine::new(Config::new().wasm_function_references(true).wasm_gc(true)).unwrap();
+fn wasmtime_runner(wasm: Vec<u8>) -> Result<Run> {
+    let engine = Engine::new(Config::new().wasm_function_references(true).wasm_gc(true))?;
     let mut store = Store::new(&engine, vec![]);
     let mut linker = Linker::new(&engine);
-    let module = Module::new(&engine, &wasm).unwrap();
-    linker
-        .func_wrap(
-            "env",
-            "log",
-            |mut caller: Caller<'_, Vec<String>>, param: i32| {
-                caller.data_mut().push(format!("{param}"));
-                0
-            },
-        )
-        .unwrap();
-    linker
-        .func_wrap(
-            "env",
-            "log_int",
-            |mut caller: Caller<'_, Vec<String>>, param: i32| {
-                caller.data_mut().push(format!("{param}"));
-                0
-            },
-        )
-        .unwrap();
-    linker
-        .func_wrap(
-            "env",
-            "log_float",
-            |mut caller: Caller<'_, Vec<String>>, param: f32| {
-                caller.data_mut().push(format!("{param}"));
-                0
-            },
-        )
-        .unwrap();
-    linker
-        .func_wrap(
-            "env",
-            "log_f32",
-            |mut caller: Caller<'_, Vec<String>>, param: f32| {
-                caller.data_mut().push(format!("{param}"));
-                0
-            },
-        )
-        .unwrap();
-    linker
-        .func_wrap(
-            "env",
-            "print_char",
-            |mut caller: Caller<'_, Vec<String>>, param: u32| {
-                let mut last_line = caller.data_mut().pop().unwrap_or_default();
-                last_line.push_str(&format!("{}", char::from_u32(param).unwrap()));
-                caller.data_mut().push(last_line);
-                0
-            },
-        )
-        .unwrap();
-    linker
-        .func_wrap("env", "random", |_: i32| -> f32 { 0.0 })
-        .unwrap();
+    let module = Module::new(&engine, &wasm)?;
+    linker.func_wrap(
+        "env",
+        "log",
+        |mut caller: Caller<'_, Vec<String>>, param: i32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        },
+    )?;
+    linker.func_wrap(
+        "env",
+        "log_int",
+        |mut caller: Caller<'_, Vec<String>>, param: i32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        },
+    )?;
+    linker.func_wrap(
+        "env",
+        "log_float",
+        |mut caller: Caller<'_, Vec<String>>, param: f32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        },
+    )?;
+    linker.func_wrap(
+        "env",
+        "log_f32",
+        |mut caller: Caller<'_, Vec<String>>, param: f32| {
+            caller.data_mut().push(format!("{param}"));
+            0
+        },
+    )?;
+    linker.func_wrap(
+        "env",
+        "print_char",
+        |mut caller: Caller<'_, Vec<String>>, param: u32| {
+            let mut last_line = caller.data_mut().pop().unwrap_or_default();
+            last_line.push_str(&format!("{}", char::from_u32(param).unwrap()));
+            caller.data_mut().push(last_line);
+            0
+        },
+    )?;
+    linker.func_wrap("env", "random", |_: i32| -> f32 { 0.0 })?;
 
-    let instance = linker.instantiate(&mut store, &module).unwrap();
+    let instance = linker.instantiate(&mut store, &module)?;
     let main_export = instance
         .exports(&mut store)
-        .find(|export| export.name().ends_with("main"))
+        .find(|export| export.name() == "main" || export.name().ends_with("::main"))
         .unwrap();
     let main_func = main_export
         .into_func()
         .unwrap()
-        .typed::<(), i32>(&mut store)
-        .unwrap();
-    match main_func.call(&mut store, ()) {
+        .typed::<(), i32>(&mut store)?;
+    let result = match main_func.call(&mut store, ()) {
         Ok(result) => Run::Success {
             result,
             output: store.into_data(),
@@ -158,20 +142,23 @@ fn wasmtime_runner(wasm: Vec<u8>) -> Run {
             reason: err.to_string(),
             output: store.into_data(),
         },
-    }
+    };
+    Ok(result)
 }
 
-fn test_run(path: &Path) {
-    let (_, paths) = collect_test_input(path);
-    let sources = read_source_files(paths).unwrap();
-    let wasm = compile_program(&sources).unwrap();
-    let output = wasmtime_runner(wasm);
-    assert_snapshot!(output)
+fn test_run(path: &Path) -> Result<()> {
+    let paths = collect_test_input(path)?;
+    let sources = read_source_files(paths)?;
+    let wasm = compile_program(&sources).map_err(|msg| anyhow!(msg))?;
+    let output = wasmtime_runner(wasm)?;
+    assert_snapshot!(output);
+    Ok(())
 }
 
-fn test_check(path: &Path) {
-    let (_, paths) = collect_test_input(path);
+fn test_check(path: &Path) -> Result<()> {
+    let paths = collect_test_input(path)?;
     assert_cmd_snapshot!(cli().env("RUST_BACKTRACE", "0").args(check_args(&paths)));
+    Ok(())
 }
 
 #[test]
@@ -179,7 +166,9 @@ fn check() {
     let cwd = std::env::current_dir().unwrap();
     glob!("check/*", |path| {
         let path = path.strip_prefix(&cwd).unwrap();
-        test_check(path);
+        test_check(path)
+            .context(format!("When checking: {}", path.display()))
+            .unwrap();
     });
 }
 
@@ -188,6 +177,8 @@ fn run() {
     let cwd = std::env::current_dir().unwrap();
     glob!("run/*", |path| {
         let path = path.strip_prefix(&cwd).unwrap();
-        test_run(path);
+        test_run(path)
+            .context(format!("When running: {}", path.display()))
+            .unwrap();
     });
 }
